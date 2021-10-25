a55348f699b1e9601e200ed61eaa0aaedaaa6831
danialedilev <danial.200316@gmail.com>
2021-10-25 12:05:28 +0600

    add prohozhdenie testa

--- src/components/Landing/ProitiTest.vue
+++ src/components/Landing/ProitiTest.vue
@@ -1,13 +1,83 @@
 <template>
-<v-container>
+<v-container fluid>
   <v-row v-if="checkIsFind">
-      <v-col>
-        <v-img :src="getImg" alt="logo"/>
+      <v-col cols="12" align-self="center" class="pa-16 grey">
+        <h3>{{test.name}}</h3>
+
       </v-col>
+    <v-col cols="12">
+      <div class="questions d-flex justify-space-between px-16 mt-5">
+<!--        <v-alert-->
+<!--            v-for="(item,index) in test.questions"-->
+<!--            :key="index"-->
+<!--            class="mr-3"-->
+<!--            style="padding: 10px 19px"-->
+<!--             width="50px"-->
+<!--             height="50px"-->
+<!--            color="blue"-->
+<!--             dark-->
+<!--        >-->
+<!--          <div class="text-h6">-->
+<!--             {{index+1}}-->
+<!--          </div>-->
+        <!--        </v-alert>-->
+        <div class="d-flex navigate__questions">
+
+        <div class="question_button_index" v-for="(items,index) in test.questions" :key="index">
+          <v-btn
+              height="50px"
+              class="mr-3"
+              depressed
+              :color="questionIndex===index||answerQuestion[index]!==null?'primary':'lighten-1'"
+
+              @click="questionIndex=index"
+          >
+            <h3>{{ index+1 }}</h3>
+          </v-btn>
+        </div>
+      </div>
+        <div class="timer">
+          <h3>{{timerMinute}}:{{timerSecund}}</h3>
+        </div>
+        </div>
+      <div class="question__content px-16 mt-10">
+          <div v-for="(item, index) in test.questions" :key="item.question" v-show="questionIndex===index">
+              <h2>{{item.question}}</h2>
+              <v-row class="ml-0">
+                <v-radio-group v-model="answerQuestion[index]">
+                  <v-radio
+                      v-for="(answer) in item.answers"
+                      :key="answer.answer"
+                      :label="`${answer.answer}`"
+                      :value="answer.answer"
+                  ></v-radio>
+                </v-radio-group>
+              </v-row>
+          </div>
+        <div v-if="+timerSecund===0&&+timerMinute===0" class="d-flex justify-end">
+          <v-btn @click="finishTest" color="primary">
+            Показать результаты
+          </v-btn>
+        </div>
+      </div>
+    </v-col>
   </v-row>
   <v-row v-else>
     <v-col>
-      <v-img :src="require('./../../assets/error.png')" alt="logo"/>
+<!--      <svg-icon :name="'../../assets/error.svg'" />-->
+      <v-img height="100%" :src="require('./../../assets/error.png')" alt="logo"/>
+    </v-col>
+  </v-row>
+  <v-row  v-show="checkIsSdanTest">
+    <v-col cols="12">
+      <div class="text-center black opacity loading">
+        <v-progress-circular
+            :size="70"
+            :width="7"
+            color="white"
+            indeterminate
+        ></v-progress-circular>
+      </div>
     </v-col>
   </v-row>
 </v-container>
@@ -15,16 +85,26 @@
 
 <script>
 import axios from "axios";
+// import SvgIcon from "../SvgIcon";
 
 export default {
   name: "ProitiTest",
+  // components: {SvgIcon},
   data:function () {
     return {
+      checkIsSdanTest:false,
+      questionIndex:0,
+      answerQuestion:[] ,
       test: null,
-      checkIsFind:true
+      checkIsFind:true,
+      timerMinute:11,
+      timerSecund:59,
     }
   },
   computed:{
+    getLength() {
+      return this.test.question.length
+    },
     getImg(){
      return  require('./../../assets/error.svg')
     }
@@ -38,18 +118,58 @@ export default {
     // })
     axios.get('http://localhost:3600/api/test/'+this.$route.params.id).then(response=>{
       this.test = response.data
+    }).then(()=>{
+      this.test.questions.forEach(()=>{
+        this.answerQuestion.splice(0,0,null)
+      })
     }).catch(error=>{
       console.log(error)
     }).finally(()=>{
       if (!this.test){
         this.checkIsFind = false
       }
+      let timerInterval = setInterval(()=>{
+        if (+this.timerSecund===0&&+this.timerMinute!==0){
+          this.timerSecund=59;
+          this.timerMinute--;
+          if(this.timerMinute<10){
+            this.timerMinute = '0'+this.timerMinute
+          }
+        }else if (+this.timerSecund!==0) {
+          this.timerSecund-=1;
+        }else {
+          console.log('cleared')
+          clearInterval(timerInterval)
+        }
+        console.log(this.timerSecund, +this.timerSecund)
+        if(+this.timerSecund<10&&this.timerSecund!=='00'){
+          this.timerSecund = '0'+this.timerSecund
+        }
+      },1000)
     })
   },
+  methods:{
+    finishTest(){
+      this.checkIsSdanTest = true
+    }
+  }
 
 }
 </script>
 
 <style scoped>
+.loading{
+  width: 100%;
+  height: 100%;
 
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  opacity: .6;
+}
 </style>
